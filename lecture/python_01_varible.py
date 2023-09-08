a0a51feb0c7f9a7871661fabe00a2c5c774bd8be
yoonyoun <144211789+yoonyoun@users.noreply.github.com>
2023-09-08 14:47:49 +0900

    Add python_01-variable

---
+++ lecture/python_01_varible.py
@@ -0,0 +1,10 @@
+# 주석(comment): 간단한 메모, 실행X
+# 1. 출력 함수(print)
+# - ()안의 값을 출력
+# - 변수값 확인 용도로 많이 사용
+printf
+
+
+# 문자열 타입(string Type)
+# - Python: '' or "" -> string
+# - c, java: ''(char), ""(String)
